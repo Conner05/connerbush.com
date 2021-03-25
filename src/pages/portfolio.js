c13d5ce410bb80c7dba14c176a9f0ae22293ca70
Conner Bush <cbush@str.com>
2021-03-24 23:54:05 -0500

    updates portfolio

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -6,7 +6,6 @@ import Layout from 'components/Layout'
 import Link from 'components/Link'
 import { useTheme } from 'components/Theming'
 import Container from 'components/Container'
-import { rhythm } from '../lib/typography'
 
 const Description = styled.p`
   margin-bottom: 10px;
@@ -21,11 +20,69 @@ export default function Index({ data: { site, allMdx } }) {
       <Container
         css={css`
           padding-bottom: 0;
+          display: flex;
+          flex-direction: column;
         `}
       >
-        <Link to="/blog" aria-label="Visit blog page">
-          View all articles
+        <h1>My Portfolio of Work</h1>
+        <Link to="https://connerbush.com" aria-label="Visit my personal blog">
+          My Personal Blog
         </Link>
+        <Description>This is my personal blog.</Description>
+        <Link
+          to="https://www.youtube.com/watch?v=EtBDSB6P4lY"
+          aria-label="Watch NashJS - Testing in React with Conner Bush"
+        >
+          NashJS - Testing in React with Conner Bush
+        </Link>
+        <Description>
+          This is a recording of a talk I presented to the NashJS user group on
+          Testing in React.
+        </Description>
+        <Link
+          to="https://str.com/data-solutions/industry-trend-report"
+          aria-label="Visit STR Trend Report Registration Process"
+        >
+          STR Trend Report Registration Process
+        </Link>
+        <Description>
+          I added online registration to the Trent Report ordering process with
+          real-time integration into Salesforce.{' '}
+          <small>Please do not register.</small>
+        </Description>
+        <Link
+          to="https://smartway.tn.gov/traffic"
+          aria-label="Visit SmartWay Traffic"
+        >
+          SmartWay Traffic
+        </Link>
+        <Description>
+          I was a member of the team that added support for Rest Areas, city
+          traffic data, marker clustering, and page bookmarking to SmartWay.
+        </Description>
+        <Link
+          to="https://apps.apple.com/us/app/christmas-4-kids/id1491062275"
+          aria-label="Visit Christmas 4 Kids iOS app"
+        >
+          Christmas 4 Kids iOS app (currently decommissioned until Nov 2021)
+        </Link>
+        <Description>
+          This is a mobile application to check in and manage users using a
+          drivers license scanner that verifies against a database that is
+          synced with a Mailchimp member list for the annual Christmas 4 Kids
+          event in Sumner County.
+        </Description>
+        <Link
+          to="https://homeful-d9f3c.firebaseapp.com/camps"
+          aria-label="Visit Homeful"
+        >
+          Homeful
+        </Link>
+        <Description>
+          Homeful is a mobile application to manage campsites for the unhoused
+          and routes for organizations that deliver food/clothing/shelter,
+          especially when multiple vehicles are involved.
+        </Description>
         <hr />
       </Container>
     </Layout>
